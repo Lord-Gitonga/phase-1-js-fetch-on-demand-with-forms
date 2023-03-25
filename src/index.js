bdb4543b445c10f6b09423d8bb750a19360a0372
lordish <gitonga403@gmail.com>
2023-03-25 11:05:33 +0300

    Complete Assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+  inputForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const input = document.querySelector('#searchByID');
+    // console.log(input.value);
+
+    fetch(`http://localhost:3000/movies/${input.value}`) // Use interpolation to get a specific value 
+    .then(function (response){
+        return response.json();
+    })
+    .then(function(data){
+        const title = document.querySelector('#movieDetails h4');
+        const summary = document.querySelector('#movieDetails p');
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+    })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
